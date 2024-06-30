b23d22c8de557c2e8eb732f6f77da2877f15f97e
T S GLOBAL <T S GLOBAL@DESKTOP-8GCR0Q0>
2024-06-29 20:59:01 -0700

    Completed Grade Calculator App

---
+++ GradeCalculator/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace GradeConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a numerical grade between 0 and 100: ");
+            int grade = Convert.ToInt32(Console.ReadLine());
+
+            if (grade < 0 || grade > 100)
+            {
+                Console.WriteLine("Invalid grade. Please enter a value between 0 and 100.");
+                return;
+            }
+
+            char letterGrade;
+            if (grade >= 90)
+            {
+                letterGrade = 'A';
+            }
+            else if (grade >= 80)
+            {
+                letterGrade = 'B';
+            }
+            else if (grade >= 70)
+            {
+                letterGrade = 'C';
+            }
+            else if (grade >= 60)
+            {
+                letterGrade = 'D';
+            }
+            else
+            {
+                letterGrade = 'F';
+            }
+
+            Console.WriteLine($"The letter grade for {grade} is {letterGrade}.");
+            Console.ReadLine();
+        }
+    }
+}
